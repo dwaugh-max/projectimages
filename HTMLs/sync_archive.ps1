b563d596565311839d9b0f90e43926d24d4b1fca
dwaugh <dwaugh@example.com>
2025-12-28 20:24:48 -0400

    Robust YouTube support and tab detection (v65.4)

---
+++ HTMLs/sync_archive.ps1
@@ -0,0 +1,28 @@
+$ka = 'c:\Github Repos\projectimages\HTMLs\knowledge_archive.md'
+$ix = 'c:\Github Repos\projectimages\HTMLs\index_LIVE.html'
+$tm = 'c:\Github Repos\projectimages\HTMLs\teachermode_LIVE.html'
+$brain = 'C:\Users\dwaug\.gemini\antigravity\brain\48e268bc-65b2-4585-9e81-43a9af3646b6\knowledge_archive.md'
+
+$head = Get-Content $ka -TotalCount 113
+# Bump version in Header to v65.4
+$head[0] = $head[0] -replace 'v65.3', 'v65.4'
+
+$idxContent = Get-Content $ix -Raw
+$idxContent = $idxContent -replace 'const API_URL = "https://script\.google\.com/[^"]+";', 'const API_URL = "[[INJECT_URL_NOW]]";'
+
+$tmContent = Get-Content $tm -Raw
+$tmContent = $tmContent -replace 'const URL = "https://script\.google\.com/[^"]+"; const PIN = "[^"]+";', 'const URL = "[[INJECT_URL_NOW]]"; const PIN = "[[INJECT_PIN_NOW]]";'
+
+$final = @($head)
+$final += '```html'
+$final += $idxContent
+$final += '```'
+$final += ''
+$final += '### **TEMPLATE C: Teacher Mode (teachermode.html)**'
+$final += '```html'
+$final += $tmContent
+$final += '```'
+
+$final | Set-Content $ka -Encoding UTF8
+Copy-Item $ka $brain -Force
+Write-Host "Sync Complete (v65.4)"
